fc7c9b7632cd1387442e3b5bfba1614135c80755
Dickson Afful <dafful.dev@gmail.com>
2024-08-22 21:37:59 -0400

    fix install

--- init.sh
+++ init.sh
@@ -4,6 +4,4 @@ yum install -y yum-utils
 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-systemctl start docker
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
